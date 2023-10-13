5acee8205ae737b29207a4d84e728eea33abbba8
OGnegin <vladislausgeorgieff@yandex.ru>
2023-10-13 16:46:54 +0300

    Create JsCode_1.js

---
+++ JsCode_1.js
@@ -0,0 +1,39 @@
+//Задание 1 
+a = prompt('Введите число'); 
+a += 1; 
+if (a === Number) {  
+	if ((a % 2) === 0) {  
+		console.log('Четное число')  
+		}; 
+	else {  console.log('Нечетное число')  
+	}; }; 
+	else {  console.log('Упс, вы ошиблись') }; 
+// Задание 
+2 x = prompt('Введите любое значение'); 
+console.log('x -' + typeof x) 
+// Задание 3 
+string = prompt('Введите сообщение'); 
+const reversString = string.split('').reduce((acc, char) => char + acc, ''); 
+console.log(reversString) 
+// Задание 4 
+let rendomNumber = Math.random() console.log(rendomNumber) 
+// Задание 5 
+let mass = [9, 6, 2, 1, 7]; 
+if lengths = mass.length; if itog for (let i = 0; i < mass.length; i = i + 1) {
+	itog = + "масса [i]" }; 
+console.log(itog) 
+// Задание 6 
+let Mass = [9, 6, 2, 1, 7]; 
+for (let i = 0; i < Mass.length; i = i + 1) { 
+	if (Mass[i] != Mass[i+1]){ console.log('ложь'); }; 
+	else {console.log('истина')  }}; 
+// Задание 7 
+let масса = [9, 6, 2, 1, 7]; 
+let ch; 
+let unch; 
+let nol; 
+if (let i = 0; i < Mass.length; i = i + 1) { 
+	if (i % 2 === 0) { ch += 1 }; 
+	else if (i % 2 != 0) { unch += 1 }; 
+	else (i === 0) { nol +=1 } } 
+console.log('Четные - ' + ch + 'Нечетные - ' + unch + 'Нули - ' + nol)
